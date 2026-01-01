ea3c842e163fbde2c630e0971a8d7148b4124eec
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2026-01-01 03:37:12 -0300

    fix(deploy) type vehicle backend

--- pass_backend/src/main.ts
+++ pass_backend/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Enable CORS
   app.enableCors();
 
